e260e42e629afcf6caaa5eb597dd30a2a0fa0841
SleepyIsaac <yxz3103@case.edu>
2025-02-21 19:33:40 -0500

    Update fastqc.sh

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -3,11 +3,11 @@
 #SBATCH -t 0-24:00 # hours:minutes for job
 #SBATCH -c 8 # number of cores requested (>= runThredN number)
 #SBATCH --mem 64G
-#SBATCH --job-name yxz3103_fastqc # Job name
+#SBATCH --job-name yxz3103_STAR_index # Job name
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=yxz3103@case.edu
-#SBATCH -o fastqc_%j.out # File to which standard out will be written
-#SBATCH -e fastqc_%j.err # File to which standard err will be written
+#SBATCH -o %j.out # File to which standard out will be written
+#SBATCH -e %j.err # File to which standard err will be written
 
 if [[ -z "$1" || -z "$2" ]]; then
     echo "Usage: sbatch $0 <FASTQ_DIR> <OUTPUT_DIR>"
@@ -20,4 +20,4 @@ OUTPUT_DIR=$2
 cd /scratch/markov2/users/yxz3103/project
 mkdir -p "$OUTPUT_DIR"
 
-/home/yxz3103/FastQC/fastqc fastq/*fastq.gz -o FASTQC_Reports
+/home/yxz3103/FastQC/fastqc "${FASTQ_DIR}"/*fastq.gz -o "$OUTPUT_DIR"
